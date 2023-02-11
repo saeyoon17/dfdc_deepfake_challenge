3c8533e8f71aeba358ebc7e485e8d9e0ff4cc2d5
Saeyoon Oh <saeyoon17@kaist.ac.kr>
2023-02-11 22:49:19 +0900

    refactor

--- train.sh
+++ train.sh
@@ -3,8 +3,8 @@
 ROOT_DIR='/input/dfdc-small-dataset/small_dataset'
 NUM_GPUS='2'
 
-python -u -m torch.distributed.launch --nproc_per_node=$NUM_GPUS --master_port 9902 ./dfdc-deepfake-challenge/training/pipelines/train_classifier.py \
- --distributed --config configs/b7.json --freeze-epochs 0 --test_every 1 --opt-level O1 --label-smoothing 0.01 --folds-csv folds.csv   --fold 0 --seed 111 --data-dir $ROOT_DIR --prefix b7_111_ > ./dfdc-deepfake-challenge/logs/b7_111_sweep_test
+python -u -m torch.distributed.launch --nproc_per_node=$NUM_GPUS --master_port 9902 ./dfdc_deepfake_challenge/training/pipelines/train_classifier.py \
+ --distributed --config configs/b7.json --freeze-epochs 0 --test_every 1 --opt-level O1 --label-smoothing 0.01 --folds-csv folds.csv   --fold 0 --seed 111 --data-dir $ROOT_DIR --prefix b7_111_ > ./dfdc_deepfake_challenge/logs/b7_111_sweep_test
 
 # python -u -m torch.distributed.launch --nproc_per_node=$NUM_GPUS --master_port 9901 training/pipelines/train_classifier.py \
 #  --distributed --config configs/b7.json --freeze-epochs 0 --test_every 1 --opt-level O1 --label-smoothing 0.01 --folds-csv folds.csv  --fold 0 --seed 555 --data-dir $ROOT_DIR --prefix b7_555_ > logs/b7_555
