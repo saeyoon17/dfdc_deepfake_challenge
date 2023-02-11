24d227460f1e89a22b5f0c8a54867dde63fd21c8
Saeyoon Oh <saeyoon17@kaist.ac.kr>
2023-02-12 02:26:48 +0900

    refactor

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@
 ROOT_DIR='/input/dfdc-small-dataset/small_dataset'
 NUM_GPUS='2'
 
-python -u -m torch.distributed.launch --nproc_per_node=$NUM_GPUS --master_port 9902 /training/pipelines/train_classifier.py \
+python -u -m torch.distributed.launch --nproc_per_node=$NUM_GPUS --master_port 9902 ./training/pipelines/train_classifier.py \
  --distributed --config configs/b7.json --freeze-epochs 0 --test_every 1 --opt-level O1 --label-smoothing 0.01 --folds-csv folds.csv   --fold 0 --seed 111 --data-dir $ROOT_DIR --prefix b7_111_ > ./logs/b7_111_sweep_test
 
 # python -u -m torch.distributed.launch --nproc_per_node=$NUM_GPUS --master_port 9901 training/pipelines/train_classifier.py \
